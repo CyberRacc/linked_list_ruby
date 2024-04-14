4fe75d9122d3c1c480ade6881b8a49f98617325b
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-04-14 23:00:44 +0100

    Add LinkedList and Node classes to implement a linked list data structure

---
+++ lib/linked_list.rb
@@ -0,0 +1,63 @@
+# frozen-string-literal: true
+
+# LinkedList class | Will represent the full list
+class LinkedList
+
+  # Adds a new node containing value to the end of the list
+  def append(value)
+
+  end
+
+  # Adds a new node containing value to the start of the list
+  def prepend(value)
+
+  end
+
+  # Returns the total number of nodes in the list
+  def size
+
+  end
+
+  # Returns the first node in the list
+  def head
+
+  end
+
+  # Returns the last node in the list
+  def tail
+
+  end
+
+  # Returns the node at the given index
+  def at(index)
+
+  end
+
+  # Removes the last element from the list
+  def pop
+
+  end
+
+  # Returns true if the passed in value is in the list and false otherwise
+  def contains?(value)
+
+  end
+
+  # Returns the index of the node containing value, or nil if not found.
+  def find(value)
+
+  end
+
+  # Represent the linked list objects as strings, so they can be printed out and previewd in the console
+  # The format should be `( value ) -> ( value) -> ( value ) -> nil`
+  def to_s
+
+  end
+end
+
+# Node class | Contains value method and a link to the next node
+class Node
+  def value
+    # Value method logic
+  end
+end
